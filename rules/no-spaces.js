9b3ec710e40ecc2e1594303d62d0c7156f0e60eb
Will Klein <will.klein@gmail.com>
2015-06-11 21:16:45 -0600

    Create ESLint plugin for indentation

---
+++ rules/no-spaces.js
@@ -0,0 +1,18 @@
+module.exports = function(context) {
+  var regex = /^[ ]+/;
+
+  return {
+    'Program': function(node) {
+      var match;
+      var lines = context.getSourceLines();
+
+      lines.forEach(function(line, i) {
+        match = regex.exec(line);
+
+        if (match) {
+          context.report(node, { line: i + 1, column: match.index + 1 }, 'Spaces found.');
+        }
+      });
+    }
+  };
+};
---
+++ tests/rules/no-tabs.js
@@ -0,0 +1,19 @@
+var linter = require('eslint').linter;
+var ESLintTester = require('eslint-tester');
+
+var eslintTester = new ESLintTester(linter);
+
+var expectedErrors = [ { message: 'Tabs found.' } ];
+
+eslintTester.addRuleTest('rules/no-tabs', {
+  valid: [
+    '  var thisHas =	"spaces";'
+  ],
+
+  invalid: [
+    {
+      code: '	var thisHas = "a tab";',
+      errors: expectedErrors
+    }
+  ]
+});
